b1f51bc4cf7481e766e676619b9a801e85c9058c
Ricky Elrod <ricky@elrod.me>
2014-05-24 07:04:25 -0400

    new blog post

---
+++ preview.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cwd="$( cd "${BASH_SOURCE[0]%/*}" && pwd )"
+
+rm -rf "$cwd/_site/" "$cwd/_cache/"
+
+if [ ! -f "$cwd/dist/build/blog/blog" ]; then
+  echo "blog binary not found."
+  exit 1
+fi
+
+"$cwd/dist/build/blog/blog" watch
