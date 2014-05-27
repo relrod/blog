888a8e689d55b1cfa059d4afe5611964a7195b83
Ricky Elrod <ricky@elrod.me>
2014-05-27 12:13:11 -0400

    new look

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-runhaskell -package-db=.cabal-sandbox/x86_64-linux-ghc-7.6.3-packages.conf.d site build && rsync -Havzre 'ssh -p222' --delete _site/ elrod.me:/srv/webmount/hakyll
+cwd="$( cd "${BASH_SOURCE[0]%/*}" && pwd )"
+"$cwd/dist/build/blog/blog" build && rsync -Havzre 'ssh -p222' --delete _site/ elrod.me:/srv/webmount/hakyll
