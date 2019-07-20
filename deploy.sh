b65a3cab1c9d1b7367c96280b2c4995dff004462
Rick Elrod <rick@elrod.me>
2019-07-19 21:23:23 -0400

    add some glamoring references
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cwd="$( cd "${BASH_SOURCE[0]%/*}" && pwd )"
-"$cwd/dist/build/blog/blog" clean && "$cwd/dist/build/blog/blog" build && rsync -Havzre 'ssh -p222' --delete _site/ elrod.me:/srv/webmount/hakyll
+"$cwd/dist/build/blog/blog" clean && "$cwd/dist/build/blog/blog" build && rsync -Havzre 'ssh' --delete _site/ relrod@hosted.elrod.me:/srv/www/elrod.me/webroot
